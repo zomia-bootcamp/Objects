6eea4c7f61d528ab08d504d3c14c827bfb31e9e9
zomia-bootcamp <132401198+zomia-bootcamp@users.noreply.github.com>
2023-05-07 23:33:12 +0300

    Update onBoard.js

--- exercises/onBoard.js
+++ exercises/onBoard.js
@@ -2,8 +2,10 @@
 
 1. Create a variable capFave and assign the captain‘s favorite food (the element in the 0th index of her 'favorite foods' array) to it. 
 Make sure to use bracket and dot notation to get the value of the food through nested access (don’t just copy the value into the variable!)
+
 2. Right now the passengers property has a value of null. Instead, assign as its value an array of objects. 
 These objects should represent the spaceship‘s passengers as individual objects. Make at least one passenger object in the array that has at least one key-value pair on it.
+
 3. Create a variable firstPassenger and assign the first passenger as its value (the element in the 0th index of the spaceship.passengers array you just made). 
 Make sure to use bracket and dot notation to get the passenger object through nested access (don’t just copy the object into the variable!)
 */
