a1b45784187114c314e8f4e5a3a70c3b3215adf7
zomia-bootcamp <132401198+zomia-bootcamp@users.noreply.github.com>
2023-05-07 22:55:19 +0300

    Create retreat.js

---
+++ exercises/retreat.js
@@ -0,0 +1,10 @@
+
+/*
+1. Create an alienShip object. It should contain a method retreat() which will console.log() the retreatMessage.
+2. Add another method to your object literal. This method, takeOff(), should console.log() the string 'Spim... Borp... Glix... Blastoff!'.
+3. Invoke your two methods: first .retreat() then .takeOff().
+*/
+
+let retreatMessage = 'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
+
+// Write your code below
