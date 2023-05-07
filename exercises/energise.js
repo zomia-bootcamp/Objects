18d34e69092fa5ffb5e36d371a26f9404aa73de5
zomia-bootcamp <132401198+zomia-bootcamp@users.noreply.github.com>
2023-05-07 23:32:58 +0300

    Update energise.js

--- exercises/energise.js
+++ exercises/energise.js
@@ -1,6 +1,8 @@
 /*
 1. Write a function greenEnergy() that has an object as a parameter and sets that object’s 'Fuel Type' property to 'avocado oil'.
+
 2. Write a function remotelyDisable() that has an object as a parameter and sets (or reassigns) that object’s disabled property to true.
+
 3. Call your two functions with the spaceship object in the code editor, then console.log() the spaceship object to confirm those properties were changed/added.
 */
 
