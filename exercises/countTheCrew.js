1576e723db89619b96beec2f2929146017e97429
zomia-bootcamp <132401198+zomia-bootcamp@users.noreply.github.com>
2023-05-07 22:34:11 +0300

    Create countTheCrew.js

---
+++ exercises/countTheCrew.js
@@ -0,0 +1,16 @@
+
+
+/*
+1. Create a variable 'crewCount' and assign the spaceship‘s numCrew property to it.
+2. Again using the dot operator, create a variable planetArray and assign the spaceship‘s flightPath property to it.
+*/
+
+let spaceship = {
+  homePlanet: 'Earth',
+  color: 'silver',
+  'Fuel Type': 'Turbo Fuel',
+  numCrew: 5,
+  flightPath: ['Venus', 'Mars', 'Saturn']
+};
+
+// Write your code below
