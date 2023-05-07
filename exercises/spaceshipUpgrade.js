2d022411478598449ed180da1a3e3b7e4fb7bb1b
zomia-bootcamp <132401198+zomia-bootcamp@users.noreply.github.com>
2023-05-07 22:50:03 +0300

    Create spaceshipUpgrade.js

---
+++ exercises/spaceshipUpgrade.js
@@ -0,0 +1,16 @@
+
+/*
+
+1. Reassign the color property of the spaceship object to have a value of 'glorious gold'
+2. Without changing lines 9 - 14, add a numEngines property with a numeric value between 1 and 10 to the spaceship object.
+3. Use the delete operator to remove the 'Secret Mission' property from the spaceship object.
+
+
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  homePlanet : 'Earth',
+  color: 'silver',
+  'Secret Mission' : 'Discover life outside of Earth.'
+};
+
+// Write your code below
