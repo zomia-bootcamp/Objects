a46bc6394b26dddded1c567e43cd8f808b231180
zomia-bootcamp <132401198+zomia-bootcamp@users.noreply.github.com>
2023-05-07 22:40:29 +0300

    Create secretMission.js

---
+++ exercises/secretMission.js
@@ -0,0 +1,19 @@
+
+
+/*
+Let’s use bracket notation to access the value of 'Active Mission' from the spaceship object in the code editor. 
+1. Create a variable isActive and assign the spaceship‘s 'Active Mission' property to it.
+2. Using bracket notation and the propName variable provided, console.log() the value of the 'Active Mission' property.
+
+
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  'Active Mission' : true,
+  homePlanet : 'Earth', 
+  numCrew: 5
+ };
+
+let propName =  'Active Mission';
+
+
+// Write your code below
